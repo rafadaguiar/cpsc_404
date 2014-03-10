a235c592feda41e9e8aa22eeccf3f6703ff37674
rafadaguiar <rfna@cin.ufpe.br>
2014-03-10 16:18:07 -0700

    initializing repo...

---
+++ assignment_1/1_createdb.sql
@@ -0,0 +1,19 @@
+-- Create Database and Table Numbers
+
+if db_id('StatisticsTest') is null
+  create database StatisticsTest;
+go
+alter database StatisticsTest set recovery simple
+go
+use StatisticsTest
+go
+
+-- Create a Numbers table
+create table Numbers(n int not null primary key);
+go
+insert Numbers(n)
+     select rn from (select row_number()
+                            over(order by current_timestamp) as rn
+                      from sys.trace_event_bindings as b1
+                          ,sys.trace_event_bindings as b2) as rd
+      where rn <= 5000000
